70ea41fc5573c7d0c26a76c844b3c1e3aa77fb6a
MIHEE PARK <44748142+PMH2906@users.noreply.github.com>
2022-05-27 21:28:19 +0900

    기능개발

---
+++ programmers_기능개발.py
@@ -0,0 +1,24 @@
+from collections import deque
+def solution(progresses, speeds):
+    answer = []
+    q = deque()
+    for i in range(len(progresses)):
+        if (100-progresses[i])%speeds[i] == 0:
+            q.append((100-progresses[i])//speeds[i])
+        else :
+            q.append(((100-progresses[i])//speeds[i])+1) 
+    
+    result = 0
+
+    while q:
+        day = q.popleft()
+        if q:
+            while day >= q[0]:
+                result+=1
+                q.popleft()
+                if not q:
+                    break
+        answer.append(result+1)
+        result = 0
+        
+    return answer
